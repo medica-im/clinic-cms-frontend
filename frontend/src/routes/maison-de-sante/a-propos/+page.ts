4a00a5f65df07927b207bd5f76854cd1a2138d77
Jerome Pinguet <jerome@jerome.cc>
2023-03-14 20:47:54 +0300

    fix ssr

--- frontend/src/routes/maison-de-sante/a-propos/+page.ts
+++ frontend/src/routes/maison-de-sante/a-propos/+page.ts
@@ -1,21 +1,13 @@
 import { variables } from '$lib/utils/constants';
 import { facilityStore } from '$lib/store/facilityStore';
 import { occupationsCardinal } from '$lib/store/workforceStore';
-import type { PageLoad } from './maison-de-sante/$types';
-
-export const prerender = true;
-export const ssr = true;
+import type { PageLoad } from './$types';
 
 const BASE_CMS_API_URI = variables.BASE_CMS_API_URI;
 
 export const load: PageLoad = async ({ fetch, params }) =>  {
-    const apiUri=`${BASE_CMS_API_URI}/pages/?type=cms.HomePage&fields=*&categories=2`;
-    const res = await fetch(apiUri);
-    const resJson = await res.json();
-    const data = resJson.items[0]
     const fData = await facilityStore.load();
     return {
-        cms: data,
         facilityData: fData,
         occupationsCardinal: await occupationsCardinal.load()
     };
