eb25a18c68311f53964b0edea017feba01605e7c
Jerome Pinguet <jerome@jerome.cc>
2023-03-19 08:41:59 +0300

    handle ssr

--- frontend/src/components/Carousel/Carousel.svelte
+++ frontend/src/components/Carousel/Carousel.svelte
@@ -7,21 +7,19 @@
 	import { browser } from '$app/environment';
 
 	const arrayCount = Array(count);
-	console.log(arrayCount);
 
 	function getImgSrc(idx: number) {
 		let newPath = path.replace(/-(\d+)\./, `-${idx}.`);
-		console.log(newPath);
 		const typeRegex = /.(\w+$)/;
 		const fileType = path.match(typeRegex)[1];
-		console.log(fileType);
 		const img = useAssets(newPath);
 		return img;
 	}
 </script>
 
+<div class="max-h-fit p-4 borderrounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 {#if browser}
-	<div class="max-h-fit p-4 borderrounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
 		<Carousel autoplay autoplayDuration={7000} duration={1000}>
 			{#each arrayCount as _, idx (idx)}<img
 					class="object-scale-down h-96 w-96"
@@ -29,5 +27,11 @@
 					alt="{alt}"
 				/>{/each}
 		</Carousel>
-	</div>
+	{:else}
+	<img
+	class="object-scale-down h-96 w-96"
+	src={getImgSrc(0)}
+	alt="{alt}"
+/>
 {/if}
+</div>
