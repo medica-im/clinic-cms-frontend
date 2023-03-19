25aa5a17a74b50817f6d0894058c506b06d41908
Jerome Pinguet <jerome@jerome.cc>
2023-03-19 08:32:04 +0300

    typo

--- frontend/src/routes/maison-de-sante/reunion-concertation-pluriprofessionnelle/+page.svelte
+++ frontend/src/routes/maison-de-sante/reunion-concertation-pluriprofessionnelle/+page.svelte
@@ -14,4 +14,4 @@
 <h2>{capitalizeFirstLetter($LL.OUTPATIENT_CLINIC.MDT_MEETING(), $language)}</h2>
 <p>Les professionnels de la MSP se réunissent une fois par mois, en équipe pluridisciplinaire, pour discuter de situations complexes dans la prise en charge de patients suivis au sein de la structure.</p>
 <p>Ces temps d’échange permettent aussi de faire le point sur le déroulement des programmes et les différentes missions de prévention afin de faire évoluer le projet de santé de la MSP.</p>
-<Carousel count={6} path="reunion-concertation-pluriprofessionnelle-0.jpg" alt={capitalizeFirstLetter($LL.OUTPATIENT_CLINIC.MDT_MEETING(), $language)} />
+<Carousel count={6} path={"reunion-concertation-pluriprofessionnelle-0.jpg"} alt={capitalizeFirstLetter($LL.OUTPATIENT_CLINIC.MDT_MEETING(), $language)} />
