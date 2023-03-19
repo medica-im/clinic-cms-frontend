b14ed88f4cc6da0e484406e45d84feb69faeb8b9
Jerome Pinguet <jerome@jerome.cc>
2023-03-19 09:15:51 +0300

    rename

--- frontend/src/lib/composables/useAssets.js
+++ frontend/src/lib/composables/useAssets.js
@@ -1,13 +1,9 @@
 export default function useAssets(asset) {
     const assets = import.meta.glob('/src/lib/assets/images/*.{jpg,jpeg,png}', {eager: true});
-    console.log(asset);
-    console.log(assets);
-    console.log(assets[asset]);
     const getAssetUrl = () => {
      if (assets[`/src/lib/assets/images/${asset}`]) {
        return assets[`/src/lib/assets/images/${asset}`].default
      }
     }
-  
     return getAssetUrl()
   }
