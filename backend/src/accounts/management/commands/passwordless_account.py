9675d64c0a290ce65719d0beb7da598d6e0c60b7
Jerome Pinguet <jerome@jerome.cc>
2023-06-06 01:37:58 +0300

    improve command line user node creation

--- backend/src/accounts/management/commands/passwordless_account.py
+++ backend/src/accounts/management/commands/passwordless_account.py
@@ -6,6 +6,8 @@ from django.db import DatabaseError, IntegrityError
 from django.contrib.sites.models import Site
 from directory.models import Slug
 from addressbook.models import Contact
+from accounts.models import GrammaticalGender
+from access.models import Role
 
 import logging
 
@@ -20,6 +22,33 @@ def validateEmail( email ):
     except ValidationError:
         return False
 
+def add_roles(edge, role):
+    role_keys = [
+        'superuser',
+        'administrator',
+        'staff',
+        'registered',
+        'anonymous',
+    ]
+    role_dict = {}
+    for k in role_keys:
+        try:
+            role_dict[k]= Role.objects.get(name=k)
+        except Role.DoesNotExist as e:
+            raise CommandError(f'Role {k} does not exist. {e}')
+            return
+    idx = role_keys.index(role)
+
+    while idx>=0:
+        for obj in edge.networkedge_organizations.all():
+            obj.roles.add(role_dict[role_keys[idx]])
+        idx-=1
+    log = [
+        f'Roles {obj.roles.all()} added to {obj}'
+        for obj in edge.networkedge_organizations.all()
+    ]
+    return log
+
 
 class Command(BaseCommand):
     help = 'Create a passwordless account using email'
@@ -32,6 +61,22 @@ class Command(BaseCommand):
         parser.add_argument('--site', type=str)
         parser.add_argument('--slug', type=str)
         parser.add_argument('--formatted_name', type=str)
+        parser.add_argument(
+            '--gender',
+            type=str,
+            choices=['F', 'M', 'N'],
+            help="grammatical gender (F, M or N)"
+        )
+        parser.add_argument(
+            '--role',
+            type=str,
+            choices=['superuser', 'administrator', 'staff', 'registered', 'anonymous'],
+            help=(
+            "minimal access role allowed ('superuser', 'administrator',"
+            "'staff', 'registered', 'anonymous')"
+            )
+        )
+
 
     def handle(self, *args, **options):
         email: str = options['email']
@@ -41,13 +86,33 @@ class Command(BaseCommand):
         if not validateEmail(email):
             raise CommandError('Email "%s" is not valid' % email)
             return
+        grammatical_gender: str = options['gender']
+        if not grammatical_gender:
+            grammatical_gender = "N"
+        try:
+            gg_instance = GrammaticalGender.objects.get(code=grammatical_gender)
+        except GrammaticalGender.DoesNotExist as e:
+            raise CommandError(
+                'GrammaticalGender does not exist. %s' % e
+            )
+            return
         User = get_user_model()
         try:
-            user, created = User.objects.get_or_create(email=email)
+            user, created = User.objects.get_or_create(
+                email=email
+            )
+            user.grammatical_gender=gg_instance
+            user.save()
             if created:
                 self.stdout.write(
                     self.style.SUCCESS(
                         'Passwordless Django user %s successfully created' % user
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.WARNING(
+                        'Passwordless Django user %s already exists' % user
                     )
                 )
         except Exception as e:
@@ -66,7 +131,7 @@ class Command(BaseCommand):
                 )
                 return
             try:
-                Slug.objects.create(
+                Slug.objects.get_or_create(
                     slug=slug,
                     site=site,
                     user=user
@@ -82,7 +147,7 @@ class Command(BaseCommand):
         if formatted_name:
             person_type=Contact.PersonType.NATURAL
             try:
-                Contact.objects.create(
+                Contact.objects.get_or_create(
                     person_type=person_type,
                     user=user,
                     formatted_name=formatted_name,
@@ -138,6 +203,13 @@ class Command(BaseCommand):
                 for edge in edges:
                     if edge.child_id == user_node.id:
                         edge.organizations.add(organization)
+                        if options['role']:
+                            log = add_roles(edge, options['role'])
+                            self.stdout.write(
+                                self.style.WARNING(
+                                    f'{log}'
+                                )
+                            )
                         edge.facilities.add(facility)
         except TypeError:
             return
