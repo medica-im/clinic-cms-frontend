06feb5d60cc95951d78e3041a1c33395e884f5fc
Jerome Pinguet <jerome@jerome.cc>
2023-06-17 23:23:19 +0300

    make link to blog external

--- src/components/Blog/BlogIconLink.svelte
+++ src/components/Blog/BlogIconLink.svelte
@@ -4,6 +4,6 @@
 	import { faBlog } from '@fortawesome/free-solid-svg-icons';
 </script>
 
-<a href={variables.BLOG_URI} title={'blog'} class="btn-icon btn-icon-sm hover:variant-soft-secondary" rel="noreferrer">
+<a href={variables.BLOG_URI} title={'blog'} class="btn-icon btn-icon-sm hover:variant-soft-secondary" rel="external">
     <span><Fa icon={faBlog} size="lg" /></span>
 </a>
