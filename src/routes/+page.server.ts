3a56ba6ee40b62f861aa2c37741630d20966ab40
Jerome Pinguet <jerome@jerome.cc>
2023-06-21 00:28:14 +0300

    env variable for ghost post count API limit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,12 @@
 import type { PageServerLoad } from './$types';
 import { getGhostData } from '$lib/Ghost/ghost';
-import { GHOST_API_KEY } from '$env/static/private';
+import { GHOST_API_KEY, GHOST_POST_COUNT } from '$env/static/private';
 export const prerender = true;
-console.log(GHOST_API_KEY);
+
+let count = parseInt(GHOST_POST_COUNT);
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
     return {
-        ghost: getGhostData(fetch, GHOST_API_KEY)
+        ghost: getGhostData(fetch, GHOST_API_KEY, count)
     };
 };
