aefd06f39140a29ae79abe89ba2cb032c58e1d1c
Jerome Pinguet <jerome@jerome.cc>
2023-08-03 03:03:27 +0300

    update svelte, kit

--- src/routes/annuaire/+page.ts
+++ src/routes/annuaire/+page.ts
@@ -1,23 +1,10 @@
-import { variables } from '$lib/utils/constants';
-import { workforceDataCached, occupations, occupationsCardinal } from '$lib/store/workforceStore';
 import { getEffectors } from '$components/Directory/directory';
 
 export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ fetch, params }) {
-    if ( variables.ORGANIZATION_CATEGORY == "msp" ) {
-        const wDC = await workforceDataCached.load();
-        const o = await occupations.load();
-        const oC = await occupationsCardinal.load()
-        return {
-            workforceDataCached: wDC,
-            occupations: o,
-            occupationsCardinal: oC
-        };
-    } else if ( variables.ORGANIZATION_CATEGORY == "cpts" ) {
-        return {
-            effectors: await getEffectors(fetch)
-        }
+export async function load({ fetch }) {
+    return { 
+        effectors: await getEffectors(fetch)
     }
 }
