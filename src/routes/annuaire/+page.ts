8dcbd20a97a7a71831070d163946618760a55460
Jerome Pinguet <jerome@jerome.cc>
2024-03-19 17:59:45 +0300

    reset selectCategories for SSR

--- src/routes/annuaire/+page.ts
+++ src/routes/annuaire/+page.ts
@@ -1,11 +1,12 @@
 import { facilityStore, websiteSchema } from '$lib/store/facilityStore';
-import { cardinalCategorizedFilteredEffectors } from '$lib/store/directoryStore';
+import { cardinalCategorizedFilteredEffectors, selectCategories } from '$lib/store/directoryStore';
 import { openGraphStore } from '$lib/store/openGraphStore';
 import type { PageLoad } from './$types';
 import { categorizedCachedEffectors } from '$lib/store/directoryStore.js';
 
 
 export const load: PageLoad = async ({ data }) => {
+    selectCategories.set([]);
     return {
         //facility: await facilityStore.load(),
         //websiteSchema: await websiteSchema.load(),
