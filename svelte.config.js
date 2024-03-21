8c35a74b16e1c42500d041d81ebb8fcfcb7f74f8
Jerome Pinguet <jerome@jerome.cc>
2024-03-21 14:24:44 +0300

    update sveltekit, svelte etc

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,6 +15,11 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+		alias: {
+			'$i18n': './src/i18n',
+			'$components': './src/components',
+			'$modals': './src/modals',
+		}
 	}
 };
 
