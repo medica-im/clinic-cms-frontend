96db2f0514ae26b77ed212aed90485d9f1723b78
Jerome Pinguet <jerome@jerome.cc>
2024-05-14 02:45:27 +0300

    enable opengraph

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 import { facilitiesWithAvatar, facilities, websiteSchema } from '$lib/store/facilityStore.ts';
 import { getAvatars } from '$lib/store/directoryStore.ts';
-//import { openGraphStore } from '$lib/store/openGraphStore';
+import { openGraphStore } from '$lib/store/openGraphStore';
 import type { PageLoad } from './$types';
 import { cardinalTypes } from '$lib/store/directoryStore';
 
@@ -10,7 +10,7 @@ export const load: PageLoad = async ({ data }) => {
         facilityCarousel: await facilitiesWithAvatar(),
         facilities: await facilities.load(),
         //websiteSchema: await websiteSchema.load(),
-        //openGraph: await openGraphStore.load(),
+        openGraph: await openGraphStore.load(),
         cardinalTypes: await cardinalTypes.load(),
         teamCarousel: await getAvatars(),
         ghost: data.ghost
