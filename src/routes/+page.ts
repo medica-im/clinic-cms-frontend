414569978a11a3aa6a32b800772958fe544863b5
Jerome Pinguet <jerome@jerome.cc>
2024-07-18 22:50:17 +0300

    add Schema tags

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
-import { websiteSchema } from '$lib/store/facilityStore';
-import { openGraphStore } from '$lib/store/openGraphStore';
+import { websiteSchema } from '$lib/store/facilityStore.ts';
+import { openGraphStore } from '$lib/store/openGraphStore.ts';
 import type { PageLoad } from './$types';
 import { categorizedFilteredEffectors } from '$lib/store/directoryStore.ts';
 
@@ -7,7 +7,7 @@ import { categorizedFilteredEffectors } from '$lib/store/directoryStore.ts';
 export const load: PageLoad = async ({ parent }) => {
     return {
         entries: await categorizedFilteredEffectors.load(),
-        //websiteSchema: await websiteSchema.load(),
+        websiteSchema: await websiteSchema.load(),
         openGraph: await openGraphStore.load(),
     };
 }
