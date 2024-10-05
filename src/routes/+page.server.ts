8f2601e74c86408c828a4b12e734a698f1673180
Jerome Pinguet <jerome@jerome.cc>
2024-10-06 02:29:39 +0300

    bump clinic-cms module version

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,14 +2,11 @@ import type { PageServerLoad } from './$types';
 import { getGhostData } from 'clinic-cms';
 import { GHOST_API_KEY, GHOST_POST_COUNT } from '$env/static/private';
 import { PUBLIC_BLOG_URI } from '$env/static/public';
-
 export const prerender = true;
 
 let count = parseInt(GHOST_POST_COUNT) || 6;
 
-let data;
-
-export const load: PageServerLoad = async ({ fetch, params }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
     return {
         ghost: await getGhostData(fetch, PUBLIC_BLOG_URI, GHOST_API_KEY, count)
     };
