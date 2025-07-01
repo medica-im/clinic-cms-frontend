c871f11cb9e341fcab442a74eb42d0ffb6f2fac3
Jerome Pinguet <jerome@jerome.cc>
2025-07-01 06:10:25 +0300

    content

--- src/routes/(programs)/prevention/canicule/Alert.svelte
+++ src/routes/(programs)/prevention/canicule/Alert.svelte
@@ -7,14 +7,16 @@
 	} from '@fortawesome/free-solid-svg-icons';
 
 	const startEventDate = new Date('2025-06-30T06:03:00.000+02:00');
-	const stopEventDate = new Date('2025-07-01T00:00:00.000+02:00');
+	const stopEventDate = new Date('2025-07-02T00:00:00.000+02:00');
 	let visible: boolean = (new Date().getTime() < stopEventDate.getTime());
 let startStr = new Intl.DateTimeFormat('fr-FR', {
-    dateStyle: 'medium',
+    dateStyle: 'full',
+	timeStyle: "short",
     timeZone: 'Europe/Paris'
 }).format(startEventDate);
 let endStr = new Intl.DateTimeFormat('fr-FR', {
-    dateStyle: 'medium',
+    dateStyle: 'full',
+	timeStyle: "short",
     timeZone: 'Europe/Paris'
 }).format(stopEventDate);
 </script>
